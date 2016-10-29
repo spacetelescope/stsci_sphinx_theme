daa2a1ea296ebfbc459b06b55e4e369660bcc471
Justin Ely <justincely@gmail.com>
2016-10-29 18:07:28 -0400

    initial try of installable theme

---
+++ README.md
@@ -0,0 +1,18 @@
+STScI theme for Sphinx docs
+===========================
+
+Borrows very heavily on astropy-bootstrap theme.
+
+Installation
+============
+
+
+In your ``conf.py`` file:
+
+.. code:: python
+
+    import stsci_sphinx_theme
+
+    html_theme = "stsci_sphinx_theme"
+
+    html_theme_path = [stsci_sphinx_theme.get_html_theme_path()]
---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+setup(
+    name='stsci_sphinx_theme',
+    version='0.0.1',
+    author='Justin Ely',
+    author_email='ely@stsci.edu',
+    packages=['stsci_sphinx_theme'],
+    package_data={'stsci_sphinx_theme': [
+                 'theme.conf',
+                 '*.html',
+                 'static/*'
+    ]},
+    include_package_data=True,
+    url='https://github.com/spacetelescope/stsci_sphinx_theme/',
+    license='BSD',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Languase :: python :: 3.5'
+    ],
+    install_requires=('sphinx>=1.1', )
+)
